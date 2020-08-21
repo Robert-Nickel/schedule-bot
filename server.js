628157bf0469cd6758b3c83790e3f20fe2d48fe3
RobertNickel <rvnickel@aikq.de>
2020-08-21 19:28:25 +0200

    Added a state the represents what the user is currently doing. If the state is "addingSubject", we process the next user input as the string that represents the subject. With this mechanism the creation of new subjects is supported.

--- server.js
+++ server.js
@@ -7,29 +7,49 @@ module.exports = botBuilder(function (message) {
   const documentClient = new AWS.DynamoDB.DocumentClient({
     region: "eu-central-1",
   });
+  return documentClient.get({
+    TableName: "Users",
+    Key: {
+      id: message.sender + "",
+    },
+  })
+    .promise()
+    .then(
+      function (data) {
+        const userData = data.Item
+        const text = message.text
 
-  if (message.text == "/subjects") {
-    var params = {
-      TableName: "Users",
-      Key: {
-        id: message.sender + "",
-      },
-    };
-    return documentClient
-      .get(params)
+        if (userData.state == "addingSubject") {
+          userData.subjects.push(text)
+          userData.state = "neutral"
+          return saveUserData(userData)
+            .then(() => { return "Added subject " + text + ". Use /subjects to see all subjects." })
+        }
+
+        else if (text == "/newSubject") {
+          userData.state = "addingSubject"
+          return saveUserData(userData)
+            .then(() => { return "Which subject do you want to create?\nPro tip: You can also add details like the room or the teacher here." })
+        }
+
+        else if (text == "/subjects") {
+          if(userData.subjects.length == 0) {
+            return "You have no subjects created yet. Use /newSubject to create one."
+          }
+          return userData.subjects;
+        }
+
+        else {
+          return "Unknown command, sorry.";
+        }
+      }
+    )
+    .catch(function (err) {
+      // TODO: If the user doesn't exist yet, create and persist a new one with the given id
+    });
+
+  function saveUserData(userData) {
+    return documentClient.put({ TableName: "Users", Item: userData })
       .promise()
-      .then(function (data) {
-        var subjects = [];
-        data.Item.subjects.forEach((subject) => {
-          subjects.push(subject.name + " bei " + subject.teacher);
-        });
-        return subjects;
-      })
-      .catch(function (err) {
-        console.log(err);
-        return "That didn‘t work :(";
-      });
-  } else {
-    return "Unknown command, sorry.";
   }
 });
