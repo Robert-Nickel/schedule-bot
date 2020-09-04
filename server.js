22d02d232f83360b3c7e194e8964ed843bed5dbc
Robert Nickel <robert.nickel@daimler.com>
2020-09-04 21:40:56 +0200

    Added /deletesubject feature.

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ module.exports = botBuilder(function (message) {
   const documentClient = new AWS.DynamoDB.DocumentClient({
     region: "eu-central-1",
   });
+  const id = message.originalRequest.message.chat.id + "";
   if (message.text == "/start") {
     return documentClient
       .put({
@@ -52,30 +53,30 @@ module.exports = botBuilder(function (message) {
 
       if (text == "/help" || text == "/start") {
         return (
-          "I will help you managing your schedule.\nCreate a /newSubject and enter details like the name of the subject, your teacher and the room." +
+          "I will help you managing your schedule.\nCreate a /newsubject and enter details like the name of the subject, your teacher and the room." +
           " Don't enter the time or day you have a subject here, the schedule will help you doing so afterwards." +
           "\nYou can see the list of all /subjects." +
-          "\nAfter having all your subjects defined, /config your schedule." + 
-          "\nYou can reset one or all days of your schedule by using /resetSchedule."
+          "\nAfter having all your subjects defined, /config your schedule." +
+          "\nYou can reset one or all days of your schedule by using /resetschedule."
         );
       } else if (userData.state == "addingSubject") {
         userData.subjects.push(text);
         userData.state = "neutral";
         return saveUserData(userData).then(() => {
           return (
-            "Added subject " +
+            "Added " +
             text +
-            ". Use /newSubject to create another one and /subjects to list all subjects. If you created all subjects, /config your schedule."
+            " to /subjects. Use /newsubject to create another one. After creating all subjects, /config your schedule."
           );
         });
       } else if (text == "/newsubject") {
         userData.state = "addingSubject";
         return saveUserData(userData).then(() => {
-          return "Which subject do you want to create?\nPro tip: You can also add details like the room or the teacher here.";
+          return "Which subject do you want to create?\nAdd details like the room or the teacher here, but not the time or day.";
         });
       } else if (text == "/subjects") {
         if (userData.subjects.length == 0) {
-          return "You have no subjects created yet. Use /newSubject to create one.";
+          return "You have no subjects created yet. Use /newsubject to create one.";
         }
         return userData.subjects;
       } else if (text == "/schedule") {
@@ -94,7 +95,7 @@ module.exports = botBuilder(function (message) {
           getNewLineSeperatedList(userData.schedule.Saturday)
         );
       } else if (text == "/resetschedule") {
-        userData.state = "resetSchedule";
+        userData.state = "resetschedule";
         var keyboard = getResetScheduleKeyboard("What do you want to reset?");
         return saveUserData(userData).then(() => {
           return keyboard;
@@ -105,15 +106,11 @@ module.exports = botBuilder(function (message) {
             userData.configCurrentWeekday = text;
             userData.schedule[text] = [];
             return saveUserData(userData).then(() => {
-              return {
-                chat_id: message.originalRequest.message.chat.id + "",
-                text:
-                  "Select your subjects on " + text + " in the right order.",
-                reply_markup: {
-                  keyboard: buildSubjectsKeyboard(userData),
-                  resize_keyboard: true,
-                },
-              };
+              return getSubjectsKeyboard(
+                userData,
+                "Select your subjects on " + text + " in the right order.",
+                "That's it"
+              );
             });
           } else {
             return getWeekdayKeyboard();
@@ -122,15 +119,11 @@ module.exports = botBuilder(function (message) {
           if (userData.subjects.includes(text)) {
             userData.schedule[userData.configCurrentWeekday].push(text);
             return saveUserData(userData).then(() => {
-              return {
-                chat_id: message.originalRequest.message.chat.id + "",
-                text:
-                  'What is your next subject? Select "That\'s it" when you are done.',
-                reply_markup: {
-                  keyboard: buildSubjectsKeyboard(userData),
-                  resize_keyboard: true,
-                },
-              };
+              return getSubjectsKeyboard(
+                userData,
+                'What is your next subject? Select "That\'s it" when you are done.',
+                "That's it"
+              );
             });
           } else if (text == "That's it") {
             userData.state = "neutral";
@@ -148,18 +141,14 @@ module.exports = botBuilder(function (message) {
               );
             });
           } else {
-            return {
-              chat_id: message.originalRequest.message.chat.id + "",
-              text:
-                'That is an unknown subject. Please select one of the following, or select "That\'s it"',
-              reply_markup: {
-                keyboard: buildSubjectsKeyboard(userData),
-                resize_keyboard: true,
-              },
-            };
+            return getSubjectsKeyboard(
+              userData,
+              'That is an unknown subject. Please select one of the following, or select "That\'s it"',
+              "That's it"
+            );
           }
         }
-      } else if (userData.state == "resetSchedule") {
+      } else if (userData.state == "resetschedule") {
         if (text == "Everything") {
           userData.schedule.Monday = [];
           userData.schedule.Tuesday = [];
@@ -170,7 +159,7 @@ module.exports = botBuilder(function (message) {
           userData.state = "neutral";
           return saveUserData(userData).then(() => {
             return getHideKeyboard(
-              "Everything was reset. Use /newSubject to create a new subject and /config your schedule afterwards."
+              "Everything was reset. Use /newsubject to create a new subject and /config your schedule afterwards."
             );
           });
         } else if (weekdays.includes(text)) {
@@ -186,9 +175,37 @@ module.exports = botBuilder(function (message) {
             "That is nothing I can reset. Please chose one of the buttons below."
           );
         }
+      } else if (text == "/deletesubject") {
+        if (userData.subjects.length < 1) {
+          return "Create a /newsubject first!"
+        }
+        userData.state = "deletingsubject";
+        return saveUserData(userData).then(() => {
+          return getSubjectsKeyboard(
+            userData,
+            "Which subject do you want to delete?",
+            "All of them!"
+          );
+        });
+      } else if (userData.state == "deletingsubject") {
+        if (userData.subjects.includes(text)) {
+          userData.state = "neutral";
+          userData.subjects.splice(userData.subjects.indexOf(text), 1);
+          return saveUserData(userData).then(() => {
+            return getHideKeyboard(text + " deleted. Create a new one with /newsubject, or see all /subjects.")
+          })
+        } else if (text == "All of them!") {
+          userData.state = "neutral";
+          userData.subjects = [];
+          return saveUserData(userData).then(() => {
+            return getHideKeyboard("All subjects deleted. Create a new one with /newsubject, or see all /subjects.")
+          })
+        } else {
+          return getSubjectsKeyboard(userData, "That subject doesn't exist, so which subject would you like to delete?", "All of them!")
+        }
       } else if (text == "/config") {
         if (userData.subjects.length < 1) {
-          return "You need to create your subjects first. Use /newSubject to create one.";
+          return "You need to create your subjects first. Use /newsubject to create one.";
         }
         userData.state = "config";
         userData.configCurrentWeekday = null;
@@ -206,19 +223,26 @@ module.exports = botBuilder(function (message) {
     return documentClient.put({ TableName: "Users", Item: userData }).promise();
   }
 
-  function buildSubjectsKeyboard(userData) {
+  function getSubjectsKeyboard(userData, text, additionalButton) {
     var setWeekdayActions = userData.subjects;
-    setWeekdayActions.push("That's it");
+    setWeekdayActions.push(additionalButton);
     var subjectsKeyboard = [];
     while (setWeekdayActions.length > 0) {
       subjectsKeyboard.push(setWeekdayActions.splice(0, 2));
     }
-    return subjectsKeyboard;
+    return {
+      chat_id: id,
+      text: text,
+      reply_markup: {
+        keyboard: subjectsKeyboard,
+        resize_keyboard: true,
+      },
+    };
   }
 
   function getWeekdayKeyboard(text) {
     return {
-      chat_id: message.originalRequest.message.chat.id + "",
+      chat_id: id,
       text: "Which weekday do you want to configure?",
       reply_markup: {
         keyboard: [
