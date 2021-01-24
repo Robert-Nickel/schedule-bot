e83e9e250b0105516f213445d36ec142c0acb0b5
Robert-Nickel <robert.nickel@aikq.de>
2021-01-24 21:55:55 +0100

    Kept working on the messageSender

--- messageSender/index.js
+++ messageSender/index.js
@@ -1,8 +1,29 @@
-const user_id = process.env.USER_ID;
-const token = process.env.BOT_TOKEN;
+"use strict";
+
+const AWS = require("aws-sdk");
 const TelegramBot = require('node-telegram-bot-api');
 
-module.exports.handler = async (event) =>{
-    const bot = new TelegramBot(token);
-    return await bot.sendMessage(user_id, 'This test message worked I guess');
-};
+exports.handler = async(event) => {
+    const bot = new TelegramBot(process.env.BOT_TOKEN)
+
+    const documentClient = new AWS.DynamoDB.DocumentClient({
+        region: "eu-central-1",
+    });
+
+    var params = {
+        TableName: 'Users',
+        FilterExpression: 'daily = :this_daily',
+        ExpressionAttributeValues: { ':this_daily': '07:00' }
+    };
+
+    await documentClient.scan(params)
+        .promise()
+        .then(data => {
+            // TODO: This is not a good state. Keep working on it.
+            const user = data.Items[0]
+            const text = 'Mondays subjects are:\n' + user.schedule["Monday"]
+            return bot.sendMessage(user.id, text)
+        })
+
+    return { "status": 200 }
+}
